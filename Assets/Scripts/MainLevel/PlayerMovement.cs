379a7d2282cf7609631a2cca07c2fed7a1a92499
Abyzls <fake_skype@abv.bg>
2022-07-15 21:39:47 +0300

    Movement improvements

--- Assets/Scripts/MainLevel/PlayerMovement.cs
+++ Assets/Scripts/MainLevel/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
    
     public Camera camera;
     public Vector2 wayPoint;
+    private float ConstZ = -10;
+    public float Offset;
 
 
     private void Start()
@@ -26,7 +28,10 @@ public class PlayerMovement : MonoBehaviour
 
         }
         
-        transform.position = Vector2.Lerp(transform.position, wayPoint, 5 * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, 10 * Time.deltaTime);
+
+        camera.transform.position = Vector3.Lerp(new Vector3(camera.transform.position.x, camera.transform.position.y, ConstZ), 
+            new Vector3(transform.position.x + Offset, transform.position.y, ConstZ), 5 * Time.deltaTime);
 
 
     }
