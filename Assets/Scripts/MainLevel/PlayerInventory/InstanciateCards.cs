016c3de803f34926c15af1c280a4018ae3f22540
Abyzls <fake_skype@abv.bg>
2022-07-17 00:11:12 +0300

    Duplicate Card Table1

--- Assets/Scripts/MainLevel/PlayerInventory/InstanciateCards.cs
+++ Assets/Scripts/MainLevel/PlayerInventory/InstanciateCards.cs
@@ -17,24 +17,26 @@ public class InstanciateCards : MonoBehaviour
 
         for (int i = 0; i < playerInventory.DiceScriptableObjects.Count; i++)
         {
-            if (hasRun < playerInventory.AddressableKeysDice.Count)
+            if (hasRun < playerInventory.DiceScriptableObjects.Count)
             {
-                if (playerInventory.DiceScriptableObjects[i].Contains(playerInventory.AddressableKeysDice[i]))
+                for (int x = 0; x < playerInventory.AddressableKeysDice.Count; x++)
                 {
-                    GameObject go = Instantiate(playerInventory.DicePrefabs[i],
-                        new Vector3(Transform.position.x + i * 230, 20, 0),
-                        Quaternion.identity);
+                    if (playerInventory.DiceScriptableObjects[i] == playerInventory.DicePrefabs[x].name)
+                    {
+                        GameObject go = Instantiate(playerInventory.DicePrefabs[x],
+                            new Vector3(Transform.position.x + i * 230, 20, 0),
+                            Quaternion.identity);
 
-                    go.transform.localScale = new Vector2(2,2);
+                        go.transform.localScale = new Vector2(2, 2);
 
-                    go.transform.parent = Canvas.transform;
+                        go.transform.parent = Canvas.transform;
 
-                    hasRun++;
+                        hasRun++;
 
+                    }
                 }
             }
         }
-
 
     }
 
